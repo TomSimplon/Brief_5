b72faa15f223546593e4755c77defd95db79bbc3
tom <tom.pustel.simplon@gmail.com>
2023-03-30 15:20:19 +0200

    Initialisation parcel

--- assets_typescript/jep.ts
+++ assets_typescript/jep.ts
@@ -4,6 +4,21 @@ class Jep extends Character {
     constructor(health: number = 100, hunger: number = 100, public water: number = 100) {
       super(health, hunger);
     }
+    decreaseJauges() {
+        if (this.health > 50) {
+          this.health -= 10;
+          this.hunger -= 10;
+          this.water -= 10;
+        } else if (this.health > 15) {
+          this.health -= 6;
+          this.hunger -= 6;
+          this.water -= 6;
+        } else if (this.health > 0) {
+          this.health -= 2;
+          this.hunger -= 2;
+          this.water -= 2;
+        }
+      }
   }
   
   export default Jep
--- assets_typescript/nina.ts
+++ assets_typescript/nina.ts
@@ -4,6 +4,21 @@ class Nina extends Character {
     constructor(health: number = 100, hunger: number = 100, public battery: number = 100) {
       super(health, hunger);
     }
+    decreaseJauges() {
+        if (this.health > 50) {
+          this.health -= 10;
+          this.hunger -= 10;
+          this.battery -= 10;
+        } else if (this.health > 15) {
+          this.health -= 6;
+          this.hunger -= 6;
+          this.battery -= 6;
+        } else if (this.health > 0) {
+          this.health -= 2;
+          this.hunger -= 2;
+          this.battery -= 2;
+        }
+      }
   }
 
   export default Nina
